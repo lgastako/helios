9451102c1abdb6776fca008ed33eaf7ac745e5b9
John Evans <john@milo.com>
2010-08-17 15:41:37 -0700

    Initial work on abstract client.

---
+++ clients/python-generic/abstractclient.py
@@ -0,0 +1,15 @@
+from Queue import Queue
+from time import time
+
+
+class AbstractHeliosClient(object):
+
+    def __init__(self):
+        self.queue = Queue()
+
+    def record(self, event, **kwargs):
+        timestamp = time()
+        self.queue.put((timestamp, event, kwargs))
+
+    def qsize(self):
+        return self.queue.qsize()
---
+++ clients/python-generic/tests.py
@@ -0,0 +1,30 @@
+"""Tests for the generic python client which is transport-independent."""
+
+import unittest
+
+from time import time
+
+from abstractclient import AbstractHeliosClient
+
+
+class NullClient(AbstractHeliosClient):
+    pass
+
+
+class AbstractHeliosClientTests(unittest.TestCase):
+
+    def test_queues_on_block(self):
+        """The client should queue requests when the transport blocks."""
+
+        client = NullClient()
+        start = time()
+        client.record("test")
+        end = time()
+        diff = end - start
+        # Is this a good enough check to confirm that it's not blocking?
+        self.assert_(diff < 0.01)
+        self.assertEquals(1, client.qsize())
+
+
+if __name__ == '__main__':
+    unittest.main()
